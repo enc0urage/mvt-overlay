6a064d6087704e6ca415426bceac84a5e708f1c0
enc0urage <127197575+enc0urage@users.noreply.github.com>
2023-03-27 13:01:34 +0000

    Fix Python 3.11 override

--- overlay.nix
+++ overlay.nix
@@ -2,7 +2,7 @@ final: prev: {
   python38 = prev.python38.override {packageOverrides = final.pythonOverrides;};
   python39 = prev.python39.override {packageOverrides = final.pythonOverrides;};
   python310 = prev.python310.override {packageOverrides = final.pythonOverrides;};
-  python311 = prev.python310.override {packageOverrides = final.pythonOverrides;};
+  python311 = prev.python311.override {packageOverrides = final.pythonOverrides;};
 
   pythonOverrides = python-self: python-super:
     with python-self; {
